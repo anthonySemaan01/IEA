85d2134ea00fd0a284c9ff5a113098bc40a4c6db
bouli <bouli@LAPTOP-0UE9A868.local>
2022-10-23 19:57:59 +0300

    Added Kfold for upper/lower + Even/Odd + Their contracts

---
+++ domain/contracts/abstract_kfold_even_odd.py
@@ -0,0 +1,7 @@
+from abc import ABC
+
+
+class Kfold_even_odd(ABC):
+
+    def kfold(self, path) -> int:
+        pass
---
+++ domain/contracts/abstract_kfold_upper_lower.py
@@ -0,0 +1,7 @@
+from abc import ABC
+
+
+class Kfold_upper_lower(ABC):
+
+    def kfold(self, path) -> int:
+        pass
